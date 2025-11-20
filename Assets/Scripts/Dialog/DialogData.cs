27becf067d3fac28c6c59a167f44aa36524b4acc
rakhasf <rakhasaptafabian@gmail.com>
2025-11-20 22:01:26 +0700

    Added Dialog and Dialog History System

---
+++ Assets/Scripts/Dialog/DialogData.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class DialogData
+{
+    public string npcName;
+    public string npcPortrait;
+    public float typingSpeed = 0.05f;
+    public DialogEntry[] dialog;
+}
+
+[Serializable]
+public class DialogEntry
+{
+    public string text;
+    public string playerChoice;
+    public string[] npcReply;
+    public string[] choices;
+}
