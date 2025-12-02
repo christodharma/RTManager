30485c2c21d768366407a5fd0297e6dba92c2bf1
christodharma <christodharma@gmail.com>
2025-12-02 16:16:29 +0700

    feat: save/load events add save/load in build scene

--- Assets/Scripts/Persistence System/PersistenceManager.cs
+++ Assets/Scripts/Persistence System/PersistenceManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class PersistenceManager : MonoBehaviour
 {
     public GameData GameData;
+
     private StorageType _storageType;
     public StorageType StorageType
     {
@@ -28,7 +30,12 @@ public class PersistenceManager : MonoBehaviour
         }
     }
     public IStorageHandler StorageHandler;
+
     List<IPersistable> Subscribers = new();
+    public event Action LoadStart;
+    public event Action LoadEnd;
+    public event Action SaveStart;
+    public event Action SaveEnd;
     public static PersistenceManager Instance { get; private set; }
 
     void Awake()
@@ -52,22 +59,30 @@ public class PersistenceManager : MonoBehaviour
     [ContextMenu("Trigger Load Game")]
     public void TriggerLoad()
     {
+        LoadStart?.Invoke();
+
         GameData = StorageHandler.Read("save");
         foreach (var item in Subscribers)
         {
             item.Load(GameData);
         }
         // Optional: reload scene
+
+        LoadEnd?.Invoke();
     }
 
     [ContextMenu("Trigger Save Game")]
     public void TriggerSave()
     {
+        SaveStart?.Invoke();
+
         foreach (var item in Subscribers)
         {
             item.Save(ref GameData);
         }
         StorageHandler.Write(GameData);
+
+        SaveEnd?.Invoke();
     }
 
     public void AddSubcriber(IPersistable persistable)
